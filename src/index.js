e2adbf21a80c678baa540fe78eb1a5b12b7203ab
GrimdvlLaptop <grimdvl@gmail.com>
2024-11-11 08:06:21 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,101 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+const initialState = {value: 0};
+
+const reducer = (state = initialState, action) => {
+    // if (action.type === 'INC') {
+    //     return state +1;
+    // }
+    // return 0;
+    switch (action.type) {
+        case 'INC':
+            // return state + 1;
+            return {
+                ...state,
+                value: state.value + 1
+            };
+        case 'DEC':
+            // return state - 1;
+            return {
+                ...state,
+                value: state.value - 1
+            };
+        case 'RND':
+            // return state * action.payload;
+            return {
+                ...state,
+                value: state.value * action.payload
+            };
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+
+const update = () => {
+    document.getElementById('counter').textContent = store.getState().value;
+}
+
+store.subscribe(update);
+
+// const inc = () => {
+//     return {
+//         type: 'INC'
+//     }
+// }
+
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const rnd = (value) => ({type: 'RND', payload: value});
+
+store.subscribe(() => {
+    console.log(store.getState());
+})
+
+document.getElementById('inc').addEventListener('click', () => {
+    store.dispatch(inc());
+});
+
+document.getElementById('dec').addEventListener('click', () => {
+    store.dispatch(dec());
+});
+
+document.getElementById('rnd').addEventListener('click', () => {
+    const value = Math.floor(Math.random() * 10)
+    store.dispatch(rnd(value));
+});
+
+// document.getElementById('inc').addEventListener('click', () => {
+//     store.dispatch({type: 'INC'});
+// });
+
+// document.getElementById('dec').addEventListener('click', () => {
+//     store.dispatch({type: 'DEC'});
+// });
+
+// document.getElementById('rnd').addEventListener('click', () => {
+//     const value = Math.floor(Math.random() * 10)
+//     store.dispatch({type: 'RND', payload: value});
+// });
+
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});
+
+// console.log(store.getState());
+
+// let state = reducer(initialState, {type: 'INC'});
+// state = reducer(state, {type: 'INC'});
+// state = reducer(state, {type: 'INC'})
+// console.log(state);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <>
+        
+        </>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
